8e91507c772fadc2fbee73a32ce75cfa45498e13
YassinEzaouibi <zouibiyassin03@gmail.com>
2024-08-05 22:52:18 +0100

    commit 5

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const questionnaire = require("./routes/api/questionnaire");
 
 // CORS middleware
 app.use(cors({
-    origin: ['https://my-floussi.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://my-floussi.vercel.app/', 'http://localhost:5173', '*'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
