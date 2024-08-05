2f614a7fc78d89a3e988200cf9f94bb9b9985db6
YassinEzaouibi <zouibiyassin03@gmail.com>
2024-08-05 19:44:40 +0100

    .env settuped

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
-    mongoURI: "mongodb+srv://zouibizaid:2sQpDXp8yusE1YIi@zaid-cluster.u22mdap.mongodb.net/?retryWrites=true&w=majority&appName=zaid-cluster",
-    secretOrKey: "secret",
-};
+    module.exports = {
+        mongoURI: process.env.MONGO_DB_URI,
+        secretOrKey: process.env.SECRET_KEY,
+    };
